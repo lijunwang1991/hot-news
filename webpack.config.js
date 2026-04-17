963ca8905839851b1c788335591f4e6a3652dc66
lijunwang1991 <lijun.wang@lincolnuni.ac.nz>
2026-04-17 20:13:11 +0800

    Fix Vercel deployment config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = (env, argv) => {
     entry: './src/index.tsx',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      publicPath: '/'
     },
     module: {
       rules: [
